7372a9ece94252895b4dea74c349a1e7be9d54d2
Sagar Sudhakar <sagarsudhakar@live.com>
2018-08-30 10:17:15 -0500

    Setting Up API key extraction from an environment file

---
+++ src/main/java/com/dwolla/sagar/ExtractKey.java
@@ -0,0 +1,32 @@
+package com.dwolla.sagar;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ExtractKey {
+
+    private static String readFileAsString(String fileName)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+
+        //String Path = "C:\\Users\\ss\\Desktop\\WeatherApp\\src\\main\\java\\com\\dwolla\\sagar\\env\\Key.txt";
+        //String data = readFileAsString(Path);
+        //System.out.println(data);
+        //String basePath = new File("").getAbsolutePath();
+        //System.out.println(basePath);
+
+        String path = new File("src/main/java/environment/Key.txt").getAbsolutePath();
+
+        System.out.println(path);
+
+        String data = readFileAsString(path);
+
+        System.out.println(data);
+    }
+}
